b35cc46c7d2a025e14fa6dd4264ee81afe84ebd4
kam <18344171365@139.com>
2024-12-26 10:41:49 +0800

    gtag

--- app/plugins/vue-gtag.js
+++ app/plugins/vue-gtag.js
@@ -1,9 +1,8 @@
-// plugins/vue-gtag.js
-import Vue from 'vue';
+import { defineNuxtPlugin } from '#app';
 import VueGtag from 'vue-gtag';
 
-export default ({ app }) => {
-  Vue.use(VueGtag, {
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
     config: { id: 'G-KF52CV9V7F' } // 替换为您的 Google Analytics ID
-  }, app.router);
-};
+  });
+});
