b6289c7662dde83340ee3935414d3f591b3cb889
kam <18344171365@139.com>
2024-12-26 12:16:03 +0800

    Monetag In-Page Push

--- app/plugins/vue-gtag.js
+++ app/plugins/vue-gtag.js
@@ -6,3 +6,6 @@ export default defineNuxtPlugin((nuxtApp) => {
     config: { id: 'G-KF52CV9V7F' } // 替换为您的 Google Analytics ID
   });
 });
+
+// 添加广告代码
+(function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('vemtoutcheeg.com',8703779,document.createElement('script'))
